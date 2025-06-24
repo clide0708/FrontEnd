9e35c7744de8550bd408c613336b094ad5614905
davizin-ra <daviramos1703@gmail.com>
2025-06-23 23:18:12 -0300

    clone

--- btnvoltar.php
+++ btnvoltar.php
@@ -1,4 +1,4 @@
-<div class="voltahead">
+<div class="container">
     <button class="botao-voltar" onclick="history.back()">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
             <path d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.498.498 0 0 1-.106-.163.498.498 0 0 1 0-.382.498.498 0 0 1 .106-.163l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z" />
--- criatreino.php
+++ criatreino.php
@@ -2,78 +2,79 @@
 <html lang="en">
 
 <head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet">
-
-  <title>CF - Inicio</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+  <title>CF - Início</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Additional CSS Files -->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/fontawesome.css">
-  <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css">
-  <link rel="stylesheet" href="assets/css/owl.css">
-  <link rel="stylesheet" href="assets/css/animate.css">
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="assets/css/fontawesome.css" />
+  <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css" />
+  <link rel="stylesheet" href="assets/css/owl.css" />
+  <link rel="stylesheet" href="assets/css/animate.css" />
   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-  <!--
-
-TemplateMo 579 Cyborg Gaming
-
-https://templatemo.com/tm-579-cyborg-gaming
-
--->
 </head>
 
-<body>
+<body class="criatreinopage">
 
-  <!-- ***** Preloader Start ***** -->
-  <div id="js-preloader" class="js-preloader">
-    <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
+  <!-- MODAL DE EDIÇÃO -->
+  <div class="modal fade" id="editarExercicioModal" tabindex="-1" role="dialog" aria-labelledby="editarExercicioModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="editarExercicioModalLabel">Editar Exercício</h5>
+        </div>
+        <div class="modal-body">
+          <form id="formEditarExercicio">
+            <input type="hidden" id="linhaAtual">
+            <div class="form-group">
+              <label for="musculoEdit">Músculo</label>
+              <input type="text" class="form-control" id="musculoEdit" required />
+            </div>
+            <div class="form-group">
+              <label for="exercicioEdit">Exercício</label>
+              <input type="text" class="form-control" id="exercicioEdit" required />
+            </div>
+            <div class="form-group">
+              <label for="seriesEdit">Séries</label>
+              <input type="number" class="form-control" id="seriesEdit" required />
+            </div>
+            <div class="form-group">
+              <label for="repeticoesEdit">Repetições</label>
+              <input type="number" class="form-control" id="repeticoesEdit" required />
+            </div>
+            <button type="submit" class="btn btn-primary">Salvar</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
-  <!-- ***** Preloader End ***** -->
 
-  <!-- ***** Header Area Start ***** -->
-
-  <?php include 'header.php'; ?>
-
-  <!-- ***** Header Area End ***** -->
+  <?php include 'btnvoltar.php'; ?>
 
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
         <div class="page-content">
-
-          <!-- ***** Banner Start ***** -->
           <h4 class="churrasqueira">Montar treino</h4>
-          <!-- ***** Banner End ***** -->
-
-          <!-- ***** Most Popular Start ***** -->
-
           <div class="most-popular">
             <div class="row">
               <div class="col-lg-12">
                 <form class="form-treino">
-                  <!-- Selecionar Músculos -->
                   <div class="form-group">
-                    <label for="Músculoss">Músculos</label>
-                    <input type="text" id="Músculoss" name="Músculoss" placeholder="--" required>
+                    <label for="musculos">Músculos</label>
+                    <select id="exercicio" name="exercicio">
+                      <option value="">Selecione</option>
+                      <option value="agachamento">Pernas</option>
+                      <option value="supino">Peitoral</option>
+                      <option value="abdominal">Abdômen</option>
+                    </select>
                   </div>
-
-                  <!-- Selecionar Exercício -->
                   <div class="form-group">
                     <label for="exercicio">Exercício</label>
                     <select id="exercicio" name="exercicio">
@@ -83,37 +84,31 @@ https://templatemo.com/tm-579-cyborg-gaming
                       <option value="abdominal">Abdominal</option>
                     </select>
                   </div>
-
-                  <!-- Número de Séries -->
                   <div class="form-group">
                     <label for="series">Séries</label>
-                    <input type="number" id="series" name="series" min="1" placeholder="0">
+                    <input type="number" id="series" name="series" min="1" placeholder="0" />
                   </div>
-
-                  <!-- Número de Repetições -->
                   <div class="form-group">
                     <label for="repeticoes">Repetições</label>
-                    <input type="number" id="repeticoes" name="repeticoes" min="1" placeholder="0">
+                    <input type="number" id="repeticoes" name="repeticoes" min="1" placeholder="0" />
                   </div>
-
-                  <!-- Botão -->
                   <div class="form-group">
-                    <button type="submit">Adicionar ao treino</button>
+                    <button type="submit" class="btn btn-success">Adicionar ao treino</button>
                   </div>
                 </form>
               </div>
             </div>
           </div>
 
-
           <div class="tabela-container">
-            <table class="farofa" id="tabela-treinos">
+            <table class="table farofa" id="tabela-treinos">
               <thead>
                 <tr>
                   <th>Músculos</th>
                   <th>Exercício</th>
                   <th>Séries</th>
                   <th>Repetições</th>
+                  <th>Ações</th>
                 </tr>
               </thead>
               <tbody>
@@ -122,136 +117,82 @@ https://templatemo.com/tm-579-cyborg-gaming
                   <td>Supino</td>
                   <td>3</td>
                   <td>10</td>
+                  <td>
+                    <button class="btn btn-primary btn-editar" data-toggle="modal" data-target="#editarExercicioModal" onclick="abrirModalEditar(this)">Editar</button>
+                    <button class="btn btn-danger btn-excluir" onclick="excluirLinha(this)">Excluir</button>
+                  </td>
                 </tr>
                 <tr>
                   <td>Quadríceps</td>
                   <td>Agachamento</td>
                   <td>4</td>
                   <td>12</td>
+                  <td>
+                    <button class="btn btn-primary btn-editar" data-toggle="modal" data-target="#editarExercicioModal" onclick="abrirModalEditar(this)">Editar</button>
+                    <button class="btn btn-danger btn-excluir" onclick="excluirLinha(this)">Excluir</button>
+                  </td>
                 </tr>
               </tbody>
             </table>
           </div>
 
-          <!-- NOME DO TREINO E BOTÃO DE ENVIAR -->
           <div class="form-group mt-3">
             <h5 class="churrasqueira">Nome do Treino</h5>
-            <input type="text" id="nomeTreino" placeholder="Ex: Treino A" required>
-            <button class="bigmac" onclick="window.location.href='veraluno.php'" style="cursor: pointer">Enviar
-              Treino</button>
+            <input type="text" id="nomeTreino" placeholder="Ex: Treino A" required />
+            <button class="bigmac btn btn-primary mt-2" onclick="window.location.href='veraluno.php'" style="cursor: pointer">Criar Treino</button>
           </div>
-
         </div>
       </div>
     </div>
-    <!-- ***** Most Popular End ***** -->
-
-    <!-- ***** Gaming Library Start ***** -->
-    <!-- <div class="gaming-library">
-            <div class="col-lg-12">
-              <div class="heading-section">
-                <h4><em>Treinos acessíveis</em> -- </h4>
-              </div>
-              <div class="item">
-                <ul>
-                  <li><img src="assets/images/game-01.jpg" alt="" class="templatemo-item"></li>
-                  <li>
-                    <h4>Dota 2</h4><span>Sandbox</span>
-                  </li>
-                  <li>
-                    <h4>Date Added</h4><span>24/08/2036</span>
-                  </li>
-                  <li>
-                    <h4>Hours Played</h4><span>634 H 22 Mins</span>
-                  </li>
-                  <li>
-                    <h4>Currently</h4><span>Downloaded</span>
-                  </li>
-                  <li>
-                    <div class="main-border-button border-no-active"><a href="#">Donwloaded</a></div>
-                  </li>
-                </ul>
-              </div>
-              <div class="item">
-                <ul>
-                  <li><img src="assets/images/game-02.jpg" alt="" class="templatemo-item"></li>
-                  <li>
-                    <h4>Fortnite</h4><span>Sandbox</span>
-                  </li>
-                  <li>
-                    <h4>Date Added</h4><span>22/06/2036</span>
-                  </li>
-                  <li>
-                    <h4>Hours Played</h4><span>740 H 52 Mins</span>
-                  </li>
-                  <li>
-                    <h4>Currently</h4><span>Downloaded</span>
-                  </li>
-                  <li>
-                    <div class="main-border-button"><a href="#">Donwload</a></div>
-                  </li>
-                </ul>
-              </div>
-              <div class="item last-item">
-                <ul>
-                  <li><img src="assets/images/game-03.jpg" alt="" class="templatemo-item"></li>
-                  <li>
-                    <h4>CS-GO</h4><span>Sandbox</span>
-                  </li>
-                  <li>
-                    <h4>Date Added</h4><span>21/04/2036</span>
-                  </li>
-                  <li>
-                    <h4>Hours Played</h4><span>892 H 14 Mins</span>
-                  </li>
-                  <li>
-                    <h4>Currently</h4><span>Downloaded</span>
-                  </li>
-                  <li>
-                    <div class="main-border-button border-no-active"><a href="#">Donwloaded</a></div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-lg-12">
-              <div class="main-button">
-                <a href="perfil.php">Mais</a>
-              </div>
-            </div>
-          </div> -->
-    <!-- ***** Gaming Library End ***** -->
-  </div>
-  </div>
-  </div>
   </div>
 
   <footer>
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved.
-
-            <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a>
+          <p>
+            Copyright © 2036 <a href="#">Cyborg Gaming</a> Company.
+            <br />Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a>
           </p>
         </div>
       </div>
     </div>
   </footer>
 
+  <!-- SCRIPTS NA ORDEM CERTA -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
-  <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script>
+    let linhaSelecionada = null;
 
-  <script src="assets/js/isotope.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-  <script src="assets/js/tabs.js"></script>
-  <script src="assets/js/popup.js"></script>
-  <script src="assets/js/custom.js"></script>
+    function abrirModalEditar(botao) {
+      linhaSelecionada = botao.closest("tr");
+      const celulas = linhaSelecionada.querySelectorAll("td");
 
+      document.getElementById("musculoEdit").value = celulas[0].textContent;
+      document.getElementById("exercicioEdit").value = celulas[1].textContent;
+      document.getElementById("seriesEdit").value = celulas[2].textContent;
+      document.getElementById("repeticoesEdit").value = celulas[3].textContent;
+    }
 
+    document.getElementById("formEditarExercicio").addEventListener("submit", function(e) {
+      e.preventDefault();
+      const celulas = linhaSelecionada.querySelectorAll("td");
+
+      celulas[0].textContent = document.getElementById("musculoEdit").value;
+      celulas[1].textContent = document.getElementById("exercicioEdit").value;
+      celulas[2].textContent = document.getElementById("seriesEdit").value;
+      celulas[3].textContent = document.getElementById("repeticoesEdit").value;
+
+      $('#editarExercicioModal').modal('hide');
+    });
+
+    function excluirLinha(botao) {
+      const linha = botao.closest("tr");
+      linha.remove();
+    }
+  </script>
 </body>
 
 </html>
