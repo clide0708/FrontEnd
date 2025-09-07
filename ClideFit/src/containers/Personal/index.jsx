e3ae1724c393b59015ca75bb5ec49ec0c7fa9744
davizin-ra <daviramos1703@gmail.com>
2025-09-07 07:24:23 -0300

    clone

--- ClideFit/src/containers/Personal/index.jsx
+++ ClideFit/src/containers/Personal/index.jsx
@@ -1,11 +1,12 @@
 import "./style.css";
+import "../../assets/css/style.css";
 
 function Personal() {
   return (
     <div className="Personal">
       <div className="containerPS">
         <div className="SC1">
-
+          <h4 className="Titulo">Personal Fulano</h4>
         </div>
         <div className="SC2">
           <div className="SC2p1">
