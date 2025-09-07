a1a0f06b742d5f0a7424217b2ceaefb376744eeb
davizin-ra <daviramos1703@gmail.com>
2025-09-07 19:17:50 -0300

    vercel

--- Backend/Controllers/TesteController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class TesteController
-{
-    public function index()
-    {
-        header('Content-Type: application/json');
-        echo json_encode(["mensagem" => "Deu certo!!"]);
-    }
-}
